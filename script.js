f6f631a34f2038605043bef77ddb07984762b65d
ms201089 <113513178+ms201089@users.noreply.github.com>
2025-07-09 14:39:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+// Hide loader when page fully loads
+window.onload = function () {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("main-content").style.display = "block";
+};
+
+</script>
